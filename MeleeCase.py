eda54956147ed04e602008253614b81473029134
TastefullBricodes <58159652+TastefullBricodes@users.noreply.github.com>
2022-06-18 23:22:55 +0800

    Source

---
+++ MeleeCase.py
@@ -0,0 +1,46 @@
+import random
+import time
+
+Caseopens = 0
+Casetotalcost = 150
+casestartingchance = 1/100
+caseincrementalchance = 0.25/100
+meleesunlocked = 0
+
+def openacase():
+    global Caseopens
+    global Casetotalcost
+    global casestartingchance
+    global caseincrementalchance
+    global meleesunlocked
+    Caseopens += 1
+    if random.random() < casestartingchance:
+        global meleesunlocked
+        meleesunlocked += 1
+        print("You have unlocked " + str(meleesunlocked) + " melee(s) at attempt " + str(Caseopens) + "Current spending until melee:" + str(Casetotalcost*Caseopens))
+    else:
+        print("Nope,Current attemps:" + str(Caseopens))
+    casestartingchance += caseincrementalchance
+
+i = 0
+for i in range(1, 1000):
+    openacase()
+    time.sleep(0.1)
+    i += 1
+print("----------------------------------------------------------------------------------")
+time.sleep(1)
+print("Generating Report...")
+time.sleep(0.3)
+print("----------------------------------------------------------------------------------")
+print("total spent: " + str(Casetotalcost * Caseopens))
+print("total attempts: " + str(Caseopens))
+print("total melee unlocked: " + str(meleesunlocked))
+print("total melee unlocked per attempt: " + str(meleesunlocked/Caseopens))
+print("----------------------------------------------------------------------------------")
+
+
+
+
+
+
+
